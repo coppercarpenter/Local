c6cdceab510703fe041577ee8c1496c2c19be624
sheheryar <sheheryar@SHEHERYAR-PC>
2018-09-09 16:24:24 +0500

    Add project files.

---
+++ GoodtoGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoodtoGo.Startup))]
+namespace GoodtoGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Local2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Local2.Startup))]
+namespace Local2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
